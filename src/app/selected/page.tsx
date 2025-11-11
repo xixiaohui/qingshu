257af7d9ba22236a6d63c091b94015b548062eff
Frank xi <yamazake@yeah.net>
2025-11-11 11:39:27 +0800

    pagination

--- src/app/selected/page.tsx
+++ src/app/selected/page.tsx
@@ -2,11 +2,19 @@
 
 import AppAppBar from "@/components/homepage/AppAppBar";
 import Footer from "@/components/homepage/Footer";
-import { cardData } from "@/components/test/CardData";
 import MainContentCard from "@/components/test/MainContentCard";
 import AppTheme from "@/shared-theme/AppTheme";
-import { Box, Container, CssBaseline, Grid, Pagination, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  CssBaseline,
+  Grid,
+  Pagination,
+  Typography,
+} from "@mui/material";
 
+import { supabase } from "@/lib/supabaseClient";
+import { useEffect, useState } from "react";
 
 function SelectTitle() {
   return (
@@ -18,19 +26,49 @@ function SelectTitle() {
   );
 }
 
-function SelectMain(){
-  return(
+//每页显示数
+const pageLimit = 7;
+
+function SelectMain() {
+  const [blogData, setblogData] = useState<any[]>([]);
+  const [pageCount, setpageCount] = useState(0);
+
+  const [page, setPage] = useState(1); // 当前页
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+    console.log("当前页是：", value);
+  };
+
+  useEffect(() => {
+    async function load() {
+      const { data, error } = await supabase.from("blogs").select("*");
+      if (error) console.error(error);
+      else {
+        setblogData(data);
+        let pageCount = Math.ceil(data.length / pageLimit);
+        setpageCount(pageCount);
+      }
+    }
+    load();
+  }, []);
+
+  return (
     <div>
       <Grid container spacing={2} columns={12}>
-        <MainContentCard data={cardData} md={4}></MainContentCard>
+        <MainContentCard
+          data={blogData.slice((page-1)*pageLimit, pageLimit*page)}
+          md={4}
+        ></MainContentCard>
       </Grid>
 
       <Box sx={{ display: "flex", flexDirection: "row", pt: 4 }}>
         <Pagination
           hidePrevButton
           hideNextButton
-          count={5}
-          boundaryCount={5}
+          count={pageCount}
+          page={page}
+          onChange={handleChange}
         />
       </Box>
     </div>
