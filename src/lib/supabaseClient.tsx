9dfa050fd23e18fc84b97370c4ca2f5395c78e5d
Frank xi <yamazake@yeah.net>
2025-12-29 11:15:08 +0800

    change supabase

--- src/lib/supabaseClient.tsx
+++ src/lib/supabaseClient.tsx
@@ -2,4 +2,4 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!);
